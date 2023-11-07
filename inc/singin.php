7bcf731c1b549100f96226688a60ac5a369ccc02
Pasinkova Irina <suntunartist@mail.ru>
2023-11-07 14:41:50 +0300

    xcc x

--- inc/singin.php
+++ inc/singin.php
@@ -9,7 +9,9 @@ $check_user = mysqli_query($connect, "SELECT * FROM `user` WHERE `login`='$login
 if (mysqli_num_rows($check_user) > 0){
 $user = mysqli_fetch_assoc($check_user);
 $_SESSION['user'] =[
-    
+    "id" => $user['id'],
+    "login"=> $user['login'],
+
 ]
 } else{
     $_SESSION['message']='Неверный логин или пароль';
