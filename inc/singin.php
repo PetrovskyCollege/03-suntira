b22e7a64201e4df46c360f38a881d36982740709
Pasinkova Irina <suntunartist@mail.ru>
2023-11-07 14:37:42 +0300

    d

--- inc/singin.php
+++ inc/singin.php
@@ -7,7 +7,10 @@ $pass = $_POST['pass'];
 
 $check_user = mysqli_query($connect, "SELECT * FROM `user` WHERE `login`='$login' AND `password`='$pass'");
 if (mysqli_num_rows($check_user) > 0){
-
+$user = mysqli_fetch_assoc($check_user);
+$_SESSION['user'] =[
+    
+]
 } else{
     $_SESSION['message']='Неверный логин или пароль';
     header ('Location: ../index.php');
